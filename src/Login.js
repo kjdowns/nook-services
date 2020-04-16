a85157dee25c4ede4d71cdb745060abf864cb196
kjdowns <kevin.downs04@gmail.com>
2020-04-15 21:37:55 -0400

    add form to login page

--- src/Login.js
+++ src/Login.js
@@ -4,7 +4,22 @@ class Login extends Component {
 
     render(){
         return (
-            <h1>Login Page</h1>
+            <div>
+                <h1>Login Page</h1>
+                <form>
+                    <div>
+                        <label>Username: </label>
+                        <input type="text" name="username" />
+                    </div>
+                    <div>
+                        <label>Password: </label>
+                        <input type="password" name="password" />
+                    </div>
+                    <div>
+                        <input type="submit" value="Login" />
+                    </div>
+                </form>
+            </div>
         )
     }
 
