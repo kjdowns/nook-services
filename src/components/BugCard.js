f9d5b751ab20594f0ddcc2b7735354825483219e
kjdowns <kevin.downs04@gmail.com>
2020-04-15 23:37:10 -0400

    creat bug card component

---
+++ src/components/BugCard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class BugCard extends Component {
+
+    render(){
+        <div>
+            <p>Bug Card</p>
+        </div>
+    }
+
+}
+
+export default BugCard
