f5a678a6e3d8e54a5942011efd42fcfcd7ea55ac
kjdowns <kevin.downs04@gmail.com>
2020-04-15 21:26:50 -0400

    stub out bug page component

---
+++ src/Bugs.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class Bugs extends Component {
+
+    render(){
+        return(
+            <h1>Bug Page</h1>
+        )
+    }
+}
+
+export default Bugs
