de2b55a6877194984a3b08020988b350d7e0de36
kjdowns <kevin.downs04@gmail.com>
2020-04-15 21:03:16 -0400

    import router and set up root route

--- src/index.js
+++ src/index.js
@@ -7,11 +7,15 @@ import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import userReducer from './reducers/userReducer'
 
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
 const store = createStore(userReducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <Route exact path="/" component={App} />
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
