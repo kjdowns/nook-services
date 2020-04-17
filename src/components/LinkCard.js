1b4a199168b1b2e1a4664b655ee178336c170b89
kjdowns <kevin.downs04@gmail.com>
2020-04-17 17:49:08 -0400

    add render content

--- src/components/LinkCard.js
+++ src/components/LinkCard.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class LinkCard extends Component {
 
     render(){
         return (
-            <div>
-
+            <div className={ this.props.name + "-card"}>
+                <Link> to={this.props.path} {this.props.name}</Link>
             </div>
         )
     }
