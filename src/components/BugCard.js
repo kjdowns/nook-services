c6649a0991199e9d49b0bd775c2be15ef3d399ec
kjdowns <kevin.downs04@gmail.com>
2020-04-17 00:07:10 -0400

    add click event to toggle card clicked or unclicked

--- src/components/BugCard.js
+++ src/components/BugCard.js
@@ -2,10 +2,20 @@ import React, { Component } from 'react'
 
 class BugCard extends Component {
 
+    handleClick = event => {
+        if (event.target.className === "clicked") {
+            event.target.style = "background-color:grey"
+            event.target.className = "unclicked"
+        } else {
+            event.target.style = "background-color:green"
+            event.target.className = "clicked"
+        }
+    }
+
     render(){
         return (
             <div>
-                <img src={this.props.url}/>
+                <img src={this.props.url} style={{backgroundColor: 'grey'}} className="unclicked" onClick={this.handleClick}/>
                 <h3>{this.props.name}</h3>
                 <ul>
                     <li>Location: {this.props.location}</li>
