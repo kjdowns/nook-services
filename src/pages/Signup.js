1a553089ec49b86941551a200834d42a5f19b593
kjdowns <kevin.downs04@gmail.com>
2020-04-17 00:19:59 -0400

    add change and submit event handlers

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -5,14 +5,26 @@ class Signup extends Component {
     constructor(){
         super()
         this.state = {
-
+            username: "",
+            password: "",
+            passwordConfirm: ""
         }
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
     }
 
     render(){
 
     }
-    
+
 }
 
 export default Signup;
