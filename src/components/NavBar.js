fbcd0c0b376c0f64849e407af35a6d1af5130bc2
kjdowns <kevin.downs04@gmail.com>
2020-04-16 23:18:49 -0400

    add links to all pages to Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,10 @@ import { NavLink } from 'react-router-dom'
 
 const Navbar = () => 
         <div>
-            <NavLink to="/login" exact >Login</NavLink>
+            <NavLink to="/login" exact >Login </NavLink>
+            <NavLink to="/" exact >Home </NavLink>
+            <NavLink to="/bugs" exact >Bugs </NavLink>
+            <NavLink to="/fish" exact >Fish </NavLink>
         </div>
 
 export default Navbar;
