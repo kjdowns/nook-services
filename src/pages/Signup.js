8577024c747452992fedff37c9936189aef0d743
kjdowns <kevin.downs04@gmail.com>
2020-04-17 00:17:05 -0400

    create signup page component

---
+++ src/pages/Signup.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Signup extends Component {
+
+    constructor(){
+        super()
+        this.state = {
+
+        }
+    }
+
+    render(){
+
+    }
+    
+}
+
+export default Signup;
