fae36fc20a7b8d4dc15dadff547ae2afaec35952
kjdowns <kevin.downs04@gmail.com>
2020-04-18 16:43:20 -0400

    fully implement createuser function

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,6 +1,7 @@
 
 export function createUser(userData){
     return (dispatch) => {
+        dispatch({type: 'START_LOADING_USER'});
         let configObj = {
             method: "POST",
             headers: {
@@ -10,6 +11,14 @@ export function createUser(userData){
             body: JSON.stringify(userData)
         };
         
-        fetch("http://localhost:3001/users", configObj);
+        fetch("http://localhost:3001/users", configObj)
+        .then(response => response.json())
+        .then(user => dispatch({type: 'LOAD_USER', user}));
+    }
+}
+
+export function fetchUser(userId){
+    return (dispatch) => {
+
     }
 }
