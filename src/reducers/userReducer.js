143406565fdf4ba64eeb8c74db1665dbd2c94e56
kjdowns <kevin.downs04@gmail.com>
2020-04-18 16:51:43 -0400

    add startloading and loaduser action types to switch statement

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,7 +1,16 @@
 
-const userReducer = (state = {}, action) => {
+const userReducer = (state = {
+    user: {},
+    fetching: false,
+    loggedIn: false
+}, action) => {
     switch (action.type) {
-    
+        case "START_LOADING_USER":
+            return {...state, fetching: true}
+
+        case "LOAD_USER":
+            return{...state, user: action.user, fetching: false, loggedIn: true}
+
         default:
             return state;
     }
