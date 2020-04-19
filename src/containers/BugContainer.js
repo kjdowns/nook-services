bfdd85f9df01ab392be747f5621d662d859c8ac4
kjdowns <kevin.downs04@gmail.com>
2020-04-18 21:58:59 -0400

    extract render into two seperate functions for data.js or userdata sources

--- src/containers/BugContainer.js
+++ src/containers/BugContainer.js
@@ -3,13 +3,23 @@ import {bugData} from '../data'
 import BugCard from '../components/BugCard'
 
 class BugContainer extends Component {
+
+    renderBugsFromData = () => {
+        return (
+            bugData.map(bug => (
+                <BugCard name={bug.name} location={bug.location} time={bug.time} northMonths={bug.northMonths} southMonths={bug.southMonths} url={bug.url} />
+            ))
+        )
+    }
+
+    renderBugsFromUser = () => {
+
+    }
     
     render(){
         return(
             <div>
-                {bugData.map(bug => (
-                    <BugCard name={bug.name} location={bug.location} time={bug.time} northMonths={bug.northMonths} southMonths={bug.southMonths} url={bug.url} />
-                ))}
+                {this.renderBugsFromData()}
             </div>
         )
     }
