ebfed26a08b1af147115dc27442d400dd4b394d6
kjdowns <kevin.downs04@gmail.com>
2020-04-18 22:45:35 -0400

    extend login sensitive rendering to fish

--- src/containers/FishContainer.js
+++ src/containers/FishContainer.js
@@ -1,19 +1,49 @@
 import React, { Component } from 'react'
+import {connect} from 'react-redux'
 import {fishData} from '../data'
 import FishCard from '../components/FishCard'
 
 class FishContainer extends Component {
+
+    renderFish = () => {
+        if (this.props.loggedIn) {
+            return this.renderFishFromUser();
+        } else {
+            return this.renderFishFromData();
+        }
+    }
+
+    renderFishFromData = () => {
+        return (
+            fishData.map(fish => (
+                <FishCard key={fish.name} name={fish.name} location={fish.location} time={fish.time} northMonths={fish.northMonths.join(", ")} southMonths={fish.southMonths.join(", ")} url={fish.url} />
+            ))
+        )
+    }
+
+    renderFishFromUser = () => {
+        return (
+            this.props.fish.map(fish => (
+                <FishCard key={fish.id} name={fish.name} location={fish.location} time={fish.time} northMonths={fish.northMonths} southMonths={fish.southMonths} url={fish.url} />
+            ))
+        )
+    }
     
     render(){
         return(
             <div>
-                {fishData.map(fish => (
-                    <FishCard name={fish.name} location={fish.location} time={fish.time} northMonths={fish.northMonths} southMonths={fish.southMonths} url={fish.url} />
-                ))}
+                {this.renderFish()}
             </div>
         )
     }
 
 }
 
-export default FishContainer
+const mapStateToProps = (state) => {
+    return {
+        loggedIn: state.loggedIn,
+        fish: state.user.fish
+    }
+}
+
+export default connect(mapStateToProps) (FishContainer)
