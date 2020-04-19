dea70cf0f249ceb9e04bcaa1c81250dd7878a0bd
kjdowns <kevin.downs04@gmail.com>
2020-04-18 21:39:54 -0400

    create fetchuser function

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -17,8 +17,11 @@ export function createUser(userData){
     }
 }
 
-export function fetchUser(userId){
+export function fetchUser(username){
     return (dispatch) => {
-
+        dispatch({type: 'START_LOADING_USER'})
+        fetch(`http://localhost:3001/users/${username}`)
+        .then(response => response.json())
+        .then(user => dispatch({type: 'LOAD_USER', user}))
     }
 }
