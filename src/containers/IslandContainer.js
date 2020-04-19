d2e636e7176608a3e88ff2ce50fe95729231ece8
kjdowns <kevin.downs04@gmail.com>
2020-04-19 17:24:26 -0400

    add island container component

---
+++ src/containers/IslandContainer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class IslandContainer extends Component {
+
+    render(){
+        return(
+            <div>
+
+            </div>
+        )
+    }
+
+}
+
+export default IslandContainer;
