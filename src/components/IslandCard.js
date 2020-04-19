ad5fc50f68600302d5dd93a09cb391a8c6643de1
kjdowns <kevin.downs04@gmail.com>
2020-04-19 19:05:32 -0400

    complete render of component content

--- src/components/IslandCard.js
+++ src/components/IslandCard.js
@@ -5,7 +5,23 @@ class IslandCard extends Component {
     render(){
         return(
             <div>
-                <h1>Island Card</h1>
+                <table>
+                    <thead>
+                    <tr>
+                        <th>Name </th>
+                        <th>Encouter Rate </th>
+                        <th>Map </th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>{this.props.name}</td>
+                        <td>{this.props.rate}</td>
+                        <td><img src={this.props.url} height="120" width="120" /></td>
+                    </tr>
+                    </tbody>
+                </table>
+                <p>{this.props.description}</p>
             </div>
         )
     }
