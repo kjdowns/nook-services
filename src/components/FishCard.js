63b9d51f319dafb81c51ad8e0bb2d64bafa7e066
kjdowns <kevin.downs04@gmail.com>
2020-04-19 22:42:51 -0400

    extend bug styling changes to fish

--- src/components/FishCard.js
+++ src/components/FishCard.js
@@ -5,14 +5,28 @@ class FishCard extends Component {
     render(){
         return (
             <div>
-                <img src={this.props.url}/>
-                <h3>{this.props.name}</h3>
-                <ul>
-                    <li>Location: {this.props.location}</li>
-                    <li>Time: {this.props.time}</li>
-                    <li>Months (Northern): {this.props.northMonths}</li>
-                    <li>Months (Southern): {this.props.southMonths}</li>
-                </ul>
+                <table>
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th> Name </th>
+                            <th> Location </th>
+                            <th> Time </th>
+                            <th> Months (Northern) </th>
+                            <th> Months (Southern) </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><img src={this.props.url} style={{backgroundColor: 'grey'}} height="64" width="64" className="unclicked" onClick={this.handleClick}/></td>
+                            <td> {this.props.name} </td>
+                            <td> {this.props.location} </td>
+                            <td> {this.props.time} </td>
+                            <td> {this.props.northMonths} </td>
+                            <td> {this.props.southMonths} </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         )
     }
