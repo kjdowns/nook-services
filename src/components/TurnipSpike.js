abd7b037abeddc3e769a0e3501335a619c85fc07
kjdowns <kevin.downs04@gmail.com>
2020-04-19 22:07:15 -0400

    add content to be rendered in component

--- src/components/TurnipSpike.js
+++ src/components/TurnipSpike.js
@@ -5,7 +5,8 @@ class TurnipSpike extends Component {
     render(){
         return (
             <div>
-                
+                <h2>{this.props.type}</h2>
+                <img src={this.props.url} />
             </div>
         )
     }
