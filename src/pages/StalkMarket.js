11d40dcf30a98bd146ae73d87a9aa6f644353a30
kjdowns <kevin.downs04@gmail.com>
2020-04-19 21:36:17 -0400

    add stalkmarket page

---
+++ src/pages/StalkMarket.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class StalkMarket extends Component {
+
+    render(){
+        return(
+            <h1>Sow Joan's Stalk Market</h1>
+        )
+    }
+    
+}
+
+export default StalkMarket
