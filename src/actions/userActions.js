41f792c8f9421c7af9881e35aeafcc44b0cc6c1e
kjdowns <kevin.downs04@gmail.com>
2020-04-21 17:12:16 -0400

    add update bug function

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -25,3 +25,20 @@ export function fetchUser(username){
         .then(user => dispatch({type: 'LOAD_USER', user}))
     }
 }
+
+export function updateBug(bugData){
+    return (dispatch) => {
+        dispatch({type: 'START_BUG_UPDATE'})
+        let configObj = {
+            method: "PATCH",
+            headers: {
+                "Content-Type": "application/json",
+                "Accept": "application/json" 
+            },
+            body: JSON.stringify(bugData)
+        }
+        fetch(`http://localhost:3001/bugs/${bugData.id}`, configObj)
+        .then(response => response.json())
+        .then(data => dispatch({type: 'BUG_UPDATED'}))
+    }
+}
