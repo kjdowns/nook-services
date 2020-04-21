454e8c6bce79153d9ed47e1bb64e392abc3c3a5b
kjdowns <kevin.downs04@gmail.com>
2020-04-21 17:14:05 -0400

    add bug actions to reducer

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -8,7 +8,11 @@ const userReducer = (state = {
     switch (action.type) {
         case "START_LOADING_USER":
         case "START_LOADING_ISLANDS":
+        case "START_BUG_UPDATE":
             return {...state, fetching: true}
+
+        case "BUG_UPDATED":
+            return {...state, fetching: false}
 
         case "LOAD_USER":
             return {...state, user: action.user, fetching: false, loggedIn: true}
