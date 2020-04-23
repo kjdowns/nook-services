f4d64de16d6d5b9f3c6e0a94359e1f791be2e3d0
kjdowns <kevin.downs04@gmail.com>
2020-04-23 19:13:40 -0400

    add font and font size styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ class Home extends Component {
 
   render() {
     return (
-      <div>
+      <div className="home-page">
         <h1>Welcome To Nook Services!</h1>
         <img alt="nook" src="https://gamewith-en.akamaized.net/article/thumbnail/rectangle/15666.png"/>
         <p>Nook Services offers a variety of tracking and information services. Select from the options below to access a service.</p>
