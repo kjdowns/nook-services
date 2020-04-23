f685d31bfc8a40e7e3f603674c6cdadccc4d3748
kjdowns <kevin.downs04@gmail.com>
2020-04-22 21:58:42 -0400

    update text on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,8 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <h1>Nook Services</h1>
+        <h1>Welcome To Nook Services!</h1>
+        <p>Nook Services offers a variety of tracking and information services. Select from the options below to access a service.</p>
         <LinkContainer />
       </div>
     )
