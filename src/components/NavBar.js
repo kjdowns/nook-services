9f5a133264c4c903e61404e34993225e48cf8db3
kjdowns <kevin.downs04@gmail.com>
2020-04-23 21:12:41 -0400

    style navbar and implement conditional link render based on login

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,51 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { NavLink } from 'react-router-dom'
+import {connect} from 'react-redux'
 
-const Navbar = () => 
-        <div>
-            <NavLink to="/login" exact >Login </NavLink>
-            <NavLink to="signup" exact>Signup </NavLink>
-            <NavLink to="/" exact >Home </NavLink>
-            <NavLink to="/bugs" exact >Bugs </NavLink>
-            <NavLink to="/fish" exact >Fish </NavLink>
-            <NavLink to="/flowers" exact >Flowers </NavLink>
-            <NavLink to="/islands" exact>Islands </NavLink>
-            <NavLink to="/turnips" exact>Turnips </NavLink>
-        </div>
+class Navbar extends Component {
 
-export default Navbar;
+    renderNavbar = () => {
+        if (this.props.loggedIn) {
+            return (
+                <div className="navbar">
+                    <NavLink to="/" exact >Home </NavLink>
+                    <NavLink to="/bugs" exact >Bugs </NavLink>
+                    <NavLink to="/fish" exact >Fish </NavLink>
+                    <NavLink to="/flowers" exact >Flowers </NavLink>
+                    <NavLink to="/islands" exact>Islands </NavLink>
+                    <NavLink to="/turnips" exact>Turnips </NavLink>
+                </div>
+            )
+        } else {
+            return (
+                <div className="navbar">
+                    <NavLink to="/login" exact >Login </NavLink>
+                    <NavLink to="signup" exact>Signup </NavLink>
+                    <NavLink to="/" exact >Home </NavLink>
+                    <NavLink to="/bugs" exact >Bugs </NavLink>
+                    <NavLink to="/fish" exact >Fish </NavLink>
+                    <NavLink to="/flowers" exact >Flowers </NavLink>
+                    <NavLink to="/islands" exact>Islands </NavLink>
+                    <NavLink to="/turnips" exact>Turnips </NavLink>
+                </div>
+            )
+        }
+    }
+
+    render (){
+        return (
+            <>
+            {this.renderNavbar()}
+            </>
+        )
+    }
+} 
+
+const mapStateToProps = state => {
+    return {
+        loggedIn: state.loggedIn
+    }
+}
+
+export default connect(mapStateToProps) (Navbar)
+                
