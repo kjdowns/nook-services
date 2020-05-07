27343617f5a4867fe3bcedce369c789812ff52e4
kjdowns <kevin.downs04@gmail.com>
2020-05-06 23:44:37 -0400

    remove dev tools

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ import Signup from './pages/Signup';
 import Turnips from './pages/Turnips';
 import Flowers from './pages/Flowers'
 
-const store = createStore(appReducer, compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()))
+const store = createStore(appReducer, compose(applyMiddleware(thunk)))
 
 ReactDOM.render(
   <Provider store={store}>
